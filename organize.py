ef60a22cfd15c0ffa03eb097d2f53774a2f9caeb
Abatall <87725973+Abatal1@users.noreply.github.com>
2023-08-31 20:48:24 -0400

    the homework for class 112

--- organize.py
+++ organize.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from_dir = r"/Users/ayaanpathak/Downloads"
-to_dir = r"/Users/ayaanpathak/Pictures"
+to_dir = r"/Users/ayaanpathak/"
 
 list_of_files = os.listdir(from_dir)
 print(list_of_files)
@@ -11,10 +11,10 @@ for file_name in  list_of_files:
     name,ext = os.path.splitext(file_name)
     if ext == "":
         continue
-    if ext in [".gif", ".png", ".jpg", ".jfif", ".svg", ".dng", ".HEIC", ".JPG", ".PNG", ".DNG"]:
+    if ext in [".txt", ".docx", ".doc", ".pdf"]:
         path1 = from_dir + "/" + file_name
-        path2 = to_dir + "/" + "image_files"
-        path3 = to_dir + "/" + "image_files" + "/" + file_name
+        path2 = to_dir + "/" + "docs"
+        path3 = to_dir + "/" + "docs" + "/" + file_name
         if os.path.exists(path2):
             print("moving")
             shutil.move(path1,path3)
